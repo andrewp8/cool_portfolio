b904db164d9e7829849982e59d0c3b99080efda0
Andrew Pham <andrew.pham.789@gmail.com>
2022-04-09 00:35:18 -0500

    youtube <1:03:41>

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './header.css';
 import CTA from './CTA';
 import ME from '../../assets/me.png';
-
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
     return (
@@ -12,7 +12,7 @@ const Header = () => {
                 <h1>Andrew Pham</h1>
                 <h5 className="text-light">Fullstack Developer</h5>
                 <CTA />
-
+                <HeaderSocials />
                 <div className="me">
                     <img src={ME} alt="me" />
                 </div>
