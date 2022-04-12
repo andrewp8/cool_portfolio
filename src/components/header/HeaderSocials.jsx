7b204e057226b47b4f072af3d7ece81118941e6e
Andrew Pham <andrew.pham.789@gmail.com>
2022-04-12 14:21:23 -0500

    Mac - 1:16:09

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,7 +5,7 @@ import {FiDribbble} from 'react-icons/fi';
 
 const HeaderSocials = () => {
     return (
-        <div className='header_socials'>
+        <div className='header__socials'>
             <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
             <a href="https://github.com" target="_blank"><FaGithub/></a>
             <a href="https://dribbble.com" target="_blank"><FiDribbble/></a>
